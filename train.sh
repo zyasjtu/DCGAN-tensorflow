c65007661e6a59c5f7b4c017fb858b3aeb3a9dcb
zyasjtu <zyasjtu@163.com>
2019-02-21 10:15:30 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,1 @@
+python main.py --input_height 100 --input_width 100 --output_height 100 --output_width 100 --dataset AIGLE --crop False --train True --epoch 30000 --input_fname_pattern '*.jpg' --batch_size 36
